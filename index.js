2a656e86eb5990eba4ceb794c3e19cb4e985a852
amaryamunavishwakarma <98891008+amaryamunavishwakarma@users.noreply.github.com>
2022-07-10 20:09:29 +0530

    fun

---
+++ index.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("iq");
+
+
+
+
+
+
+btn.addEventListener("click",checkiq);
+
+function checkiq(){
+    let name=document.getElementById("name").value;
+    let fname=document.getElementById("fname").value;
+    window.alert("Congratulation !");
+    window.alert(name+" "+"and"+" "+fname+" "+"are Donkey");
+   window.location.href="https://media.istockphoto.com/vectors/cartoon-raven-isolated-on-white-background-vector-id597250060?s=612x612"
+}
